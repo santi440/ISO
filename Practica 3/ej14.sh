a1373760dade01d654f4dd649bc38a4cb00c1352
Matias Santiago Marcos <santiago0105@hotmail.com.ar>
2024-09-27 20:00:07 -0300

    el 14 mas lindo

--- Practica 3/ej14.sh
+++ Practica 3/ej14.sh
@@ -2,28 +2,26 @@
 if [ $# -ne 3 ] ; then
 	echo "Parametros invalidos"	
 	exit 1
-else
-	if [ -d $1 ] ; then
-		cd $1
-		echo "Me movi a `$1`"
-		if [ $2 == "-a" ] ; then
-			for elem in `ls`
-			do
-				echo $elem 
-				mv $elem $elem$3   	
-			done
-		elif [ $2 == "-b" ] ; then
-			for elem in `ls`
-			do
-				mv $elem $3$elem 
-			done
-		else
-			echo "Parametro no conocido"
-			exit 1
-		fi
-	else
-		echo $1 "No es un directorio valido "
-		exit 1
 fi
-exit 0
+if [ -d $1 ] ; then
+	cd $1
+	echo "Me movi a `$1`"
+	if [ $2 == "-a" ] ; then
+		for elem in `ls`
+		do
+			echo $elem 
+			mv $elem $elem$3   	
+		done
+	elif [ $2 == "-b" ] ; then
+		for elem in `ls`
+		do
+			mv $elem $3$elem 
+		done
+	else
+		echo "Parametro no conocido"
+		exit 1
 	fi
+else
+	echo $1 "No es un directorio valido "
+	exit 1
+fi
